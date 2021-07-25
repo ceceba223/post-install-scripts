61c8a6720be619caeeca0a92a09334f1c7372013
ceceba223 <hagen-gerlatzek@web.de>
2021-07-25 18:58:04 +0200

    pacman installs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # post-install-scripts
+A collection of all my post install scrips for diffrent linux distros.
+
+## Installed software:
+
---
+++ arch/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# pacman installs:
+sudo pacman -S bpytop dmenu etcher firefox gimp git gparted htop kitty macchanger ncdu neofetch nitrogen picom python ranger rofi shutter steam sublime-text timeshift vokoscreen yay
